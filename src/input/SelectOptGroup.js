8ed1e6727fd407525e625a874557b1f31f9d1f1e
nazaninreihani <nazanin.reyh@gmail.com>
2017-11-07 11:13:48 +0330

    add select optgroup

---
+++ src/input/SelectOptGroup.js
@@ -0,0 +1,48 @@
+import React, { PureComponent } from 'react';
+import Label from '../i18n/Label';
+
+type Option = {
+  text: string,
+  value: string,
+  disabled: boolean,
+  group: string,
+}
+
+export default class SelectOptGroup extends PureComponent {
+
+  props: {
+    hint: string,
+    id: string,
+    className: string,
+    label: string,
+    options: Option[],
+    readOnly: bool,
+    value: string,
+    onChange: (e: SyntheticEvent) => void,
+  };
+
+  static defaultProps = {
+    options: [],
+  };
+
+  render() {
+    const { className, id, label, hint, value, options, readOnly, onChange } = this.props;
+    const labels = options.map(a => a.group).filter((elem, index, self) => index === self.indexOf(elem));
+
+    return (
+      <fieldset className={className}>
+        {label && <Label htmlFor={id} text={label} />}
+        <select id={id} readOnly={readOnly} onChange={onChange} value={value}>
+          {labels.map((l) => {
+            <optgroup label={l}>
+              {options.map((o: Option) => o.group === l &&
+                <option key={o.value} value={o.value} disabled={o.disabled}>{o.text}</option>
+              )}
+            </optgroup>
+          })}
+        </select>
+        {hint && <p className="hint">{hint}</p>}
+      </fieldset>
+    );
+  }
+}
